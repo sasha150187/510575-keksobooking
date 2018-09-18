0aec7672e4229d38dbd12f8dc34611c63bbb0c29
Саша <tama_x@mail.ru>
2018-09-18 13:27:32 +0300

    new

--- js/map.js
+++ js/map.js
@@ -1,19 +1,14 @@
-'use strict';
+'use strict'
 
-// var mapFade = document.querySelector('.map');
-// mapFade.classList.remove('map--faded');
-
-var yRange = {
-  min: 130,
-  max: 630
-};
-
-var pinSize = {
-      width: 50,
-      height: 70
-};
-
+var yRange = { min: 130, max: 630 };
+var pinSize = { width: 50, height: 70 };
 var container = document.querySelector('.map__pins');
+var card = document.querySelector('#card').content.querySelector('.map__card');
+var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinsContainer = document.querySelector('.map__pins');
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 var titles = [
   'Большая уютная квартира',
@@ -59,20 +54,13 @@ var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg'
 ];
-
-var getRandomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-
-
-
+// 1 фу-я создает массив avatar (author: {avatar:)
 function createUser(i) {
   return {
     avatar: 'img/avatars/user0' + (i + 1) + '.png'
   }
 }
-// 2 фу-я
+// 2 фу-я создает массив offer
 function createRealty(i, coords) {
   return {
     title: titles[i],
@@ -90,7 +78,7 @@ function createRealty(i, coords) {
     })
   }
 }
-// 3 фу-я
+// 3 фу-я создает координаты, массив location
 function createCoords (container) {
 
   return {
@@ -98,89 +86,93 @@ function createCoords (container) {
     y: getRandomNumber(yRange.min, yRange.max)
   }
 }
-// 4 функия
-function createAds (i, container) {
-  var ads = {};
+// 4 функия создает один из 8(i) объектов внутри массива(#card)
+function createAdvertisement (i, container) {
+  var advertisement = {};
+  advertisement.author = createUser(i); // author
+  advertisement.location = createCoords(container); //location или координаты
+  var coordinate = [advertisement.location.x, advertisement.location.y];
+  advertisement.offer = createRealty(i, coordinate);
 
-  ads.author = createUser(i);
-  ads.location = createCoords(container);
-  var cords = [ads.location.x, ads.location.y];
-  ads.offer = createRealty(i, cords);
+  return advertisement;
+};
+// 5 фу-я создает один из пинов, который мы будем видеть на карте как объявление жилья
+function createPin (offers) {
+  var clonePin = pin.cloneNode(true);
+  clonePin.querySelector('img').src = offers.author.avatar;
+  clonePin.querySelector('img').alt = offers.offer.title;
+  clonePin.style.left = offers.location.x - pinSize.width/2 + 'px';
+  clonePin.style.top = offers.location.y + pinSize.height + 'px';
+  clonePin.addEventListener ('click', function () {
+    insertAfter(cardsElements, pinsContainer);
+    clonePin.classList.add('map__pin--active');
 
-  return ads;
-}
-
-// 5 фу-я
-function createPin (offer) {
-  var  template = document.querySelector('#pin').content.querySelector('.map__pin').cloneNode(true);
-  var  image = template.querySelector('img');
-  template.style.left = offer.location.x - pinSize.width/2 + 'px';
-  template.style.top = offer.location.y + pinSize.height + 'px';
-  image.src = offer.author.avatar;
-  image.alt = offer.offer.title;
-  return template;
-}
-
-function createCard (unit) {
-  var template = document.querySelector('#card').content.querySelector('.map__card').cloneNode(true),
-      user = template.querySelector('.popup__avatar'),
-      photoImage = template.querySelector('.popup__photo'),
-      photos = template.querySelector('.popup__photos'),
-      capacity = template.querySelector('.popup__text--capacity'),
-      time = template.querySelector('.popup__text--time'),
-      selectorMap = {
-        'title': '.popup__title',
-        'price': '.popup__text--price',
-        'address': '.popup__text--address',
-        'price': '.popup__text--price',
-        'type': '.popup__type',
-        'features': '.popup__features',
-        'description': '.popup__description'
-      };
-
-  user.src = unit.author.avatar;
-  photos.innerHTML = '';
-  unit.offer.photos.forEach(function(src, i) {
-    var photo = photoImage.cloneNode(true);
-    photo.src = src;
-    photos.appendChild(photo)
   });
 
-  capacity.innerHTML = unit.offer.rooms + ' комнаты для ' + unit.offer.guests + ' гостей';
-  time.innerHTML = 'Заезд после ' + unit.offer.checkin + ', выезд до' + unit.offer.checkout;
+  return clonePin;
+};
+// 6 фу-я создает одну из карт объявления
+function createCard (offers) {
+  var cloneCard = card.cloneNode(true);
+  cloneCard.querySelector('img').src = offers.author.avatar;
+  cloneCard.querySelector('.popup__title').textContent = offers.offer.title;
+  cloneCard.querySelector('.popup__text--address').textContent = offers.offer.address;
+  cloneCard.querySelector('.popup__text--price').textContent = offers.offer.price + ' ₽/ночь';
+  cloneCard.querySelector('.popup__type').textContent = offers.offer.type;
+  cloneCard.querySelector('.popup__text--capacity').textContent = offers.offer.rooms + ' комнаты для ' + offers.offer.guests + ' гостей';
+  cloneCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + offers.offer.checkin + ', выезд до' + offers.offer.checkout;
+  cloneCard.querySelector('.popup__description').textContent = offers.offer.description;
+// создадим блок features
+  var features = cloneCard.querySelector('.popup__features');
+  features.innerHTML = '';
 
-  var map = Object.keys(selectorMap)
-  for (var i = 0; i < map.length; i++) {
-    var key = map[i];
-    var element = template.querySelector(selectorMap[key]);
-    element.innerHTML = key === 'features' ?  unit.offer[key].join(', ') : unit.offer[key];
-    if (key === 'price') {
-      element.innerHTML +='₽/ночь';
-    }
-  }
+var fragment = document.createDocumentFragment();
 
-  Object.keys(selectorMap).forEach(function(key) {
-      var element = template.querySelector(selectorMap[key]);
-      element.innerHTML = key === 'features' ?  unit.offer[key].join(', ') : unit.offer[key];
-// if (key === 'features') {
-//   element.innerHTML = unit.offer[key].join(', ')
-// }
-// else {
-//   element.innerHTML = unit.offer[key];
-// }
-      if (key === 'price') {
-        element.innerHTML +='₽/ночь';
-      }
+  for (var j = 0; j < 6; j++) {
+  var newElement = document.createElement('li');
+  newElement.className = 'popup__feature popup__feature--' + facilities[j];
+  fragment.appendChild(newElement);
+  };
+  features.appendChild(fragment);
+// создадим внутри блока(.popup__photos) нужное количество img
+  var blockForPhoto = cloneCard.querySelector('.popup__photos');
+  var img = cloneCard.querySelector('.popup__photos').querySelector('img');
+  blockForPhoto.removeChild(img);
+
+  var fragment = document.createDocumentFragment();
+
+  for (var k = 0; k < 3; k++) {
+  var newElement = document.createElement('img');
+  newElement.src = photos[k];
+  newElement.style.width = "45px";
+  newElement.style.height = "45px";
+  newElement.className = 'popup__photo';
+  newElement.alt = 'Фотография жилья';
+  fragment.appendChild(newElement);
+  };
+
+  blockForPhoto.appendChild(fragment);
+// добавим обработчики события для карточки-объявления
+  var closeButton = cloneCard.querySelector('.popup__close');
+  closeButton.addEventListener('click', function () {
+    cloneCard.classList.add('hidden');
   });
-  return template;
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      cloneCard.classList.add('hidden');
+    };
+});
+
+  return cloneCard;
 };
 
 function getFragment (offers, create) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < offers.length; i++) {
-    var el = create(offers[i]);
-    fragment.appendChild(el);
+    var element = create(offers[i]);
+    fragment.appendChild(element);
   }
 
   return fragment;
@@ -188,25 +180,22 @@ function getFragment (offers, create) {
 
 function insertAfter (elem, refElem) {
   return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-}
-
-var pinsContainer = document.querySelector('.map__pins');
+};
+// цикл создающий массив из 8 объектов
 var offers = [];
 for (var i = 0; i < 8; i++) {
-  offers.push(createAds(i, pinsContainer));
+  offers.push(createAdvertisement(i, pinsContainer));
 }
 
 var pinsElements = getFragment(offers, createPin);
 var cardsElements = getFragment(offers, createCard);
-
 // insertAfter(cardsElements, pinsContainer);
 
 
 
+
 // Лекция номер 4:
-
-
-
+// активация страницы перетягиванием метки
 var mapPinButton = document.querySelector('.map__pin--main');
 mapPinButton.addEventListener('mouseup', function () {
   var mapFade = document.querySelector('.map');
@@ -217,11 +206,7 @@ mapPinButton.addEventListener('mouseup', function () {
   input.setAttribute('value', redMuffinCords.join());
   pinsContainer.appendChild(pinsElements);
 });
-
+// получение координат метки(адреса)
 var button = document.querySelector('button');
-var pinCoordinate = {
-  x: container.offsetWidth / 2,
-  y: button.offsetTop + pinSize.height / 2
-};
-
+var pinCoordinate = { x: container.offsetWidth / 2, y: button.offsetTop + pinSize.height / 2 };
 var redMuffinCords = [pinCoordinate.x, pinCoordinate.y];
