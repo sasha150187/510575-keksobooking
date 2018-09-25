0e6b39c7190e3e3b1fb3c79823372e182cd5e65c
SashaKBZ <33097756+sasha150187@users.noreply.github.com>
2018-09-25 13:20:55 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -8,20 +8,20 @@
   var card = document.querySelector('#card').content.querySelector('.map__card');
   var pin = document.querySelector('#pin').content.querySelector('.map__pin');
   var pinsContainer = document.querySelector('.map__pins');
-  var pinSize = { width: 50, height: 70 };
+  var pinSize = {width: 50, height: 70};
   // 5 фу-я создает один из пинов, который мы будем видеть на карте как объявление жилья
-  function createPin (offer) {
+  function createPin(offer) {
     var clonePin = pin.cloneNode(true);
     clonePin.querySelector('img').src = offer.author.avatar;
     clonePin.querySelector('img').alt = offer.offer.title;
     clonePin.style.left = offer.location.x - pinSize.width / 2 + 'px';
     clonePin.style.top = offer.location.y + pinSize.height + 'px';
-    clonePin.addEventListener ('click', function () {
-      if(currentCard) {
+    clonePin.addEventListener('click', function () {
+      if (currentCard) {
         currentCard.remove();
       }
       currentCard = createCard(offer);
-      if(activePin) {
+      if (activePin) {
         activePin.classList.remove('map__pin--active');
       }
       activePin = clonePin;
@@ -30,9 +30,9 @@
     });
 
     return clonePin;
-  };
+  }
   // 6 фу-я создает одну из карт объявления
-  function createCard (offers) { // в ед. числе
+  function createCard(offers) { // в ед. числе
     var cloneCard = card.cloneNode(true);
     cloneCard.querySelector('img').src = offers.author.avatar;
     cloneCard.querySelector('.popup__title').textContent = offers.offer.title;
@@ -42,28 +42,28 @@
     cloneCard.querySelector('.popup__text--capacity').textContent = offers.offer.rooms + ' комнаты для ' + offers.offer.guests + ' гостей';
     cloneCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + offers.offer.checkin + ', выезд до' + offers.offer.checkout;
     cloneCard.querySelector('.popup__description').textContent = offers.offer.description;
-  // создадим блок features
+    // создадим блок features
     var features = cloneCard.querySelector('.popup__features');
     features.innerHTML = '';
-    offers.offer.features.forEach(function(item) {
+    offers.offer.features.forEach(function (item) {
       var newElement = document.createElement('li');
       newElement.classList.add('popup__feature');
       newElement.classList.add('popup__feature--' + item);
       features.appendChild(newElement);
     });
-  // создадим внутри блока(.popup__photos) нужное количество img
+    // создадим внутри блока(.popup__photos) нужное количество img
     var blockForPhoto = cloneCard.querySelector('.popup__photos');
     blockForPhoto.innerHTML = '';
-    offers.offer.photos.forEach(function(item) {
+    offers.offer.photos.forEach(function (item) {
       var newElement = document.createElement('img');
       newElement.src = item;
-      newElement.style.width = "45px";
-      newElement.style.height = "45px";
+      newElement.style.width = '45px';
+      newElement.style.height = '45px';
       newElement.classList.add('popup__photo');
       newElement.alt = 'Фотография жилья';
       blockForPhoto.appendChild(newElement);
     });
-  // добавим обработчики события для карточки-объявления
+    // добавим обработчики события для карточки-объявления
     var closeButton = cloneCard.querySelector('.popup__close');
     closeButton.addEventListener('click', function (event) {
       event.preventDefault();
@@ -72,10 +72,10 @@
     document.addEventListener('keydown', escPressHandler);
 
     return cloneCard;
-  };
+  }
 
   function closeCard() {
-    if(activePin) {
+    if (activePin) {
       activePin.classList.remove('map__pin--active');
     }
     currentCard.remove();
@@ -85,7 +85,7 @@
   function escPressHandler(event) {
     if (event.keyCode === ESC_KEYCODE) {
       closeCard();
-    };
+    }
   }
 
   window.map = {
